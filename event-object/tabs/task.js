96d1ac8a9a26620bae8881c8e3599221bb968a8f
Svetlased <svetla-sed2@mail.ru>
2021-06-25 22:19:20 +0200

    Additions are added

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,8 +1,9 @@
 const tabs = Array.from(document.querySelectorAll(".tab"));
 const tabsContent = Array.from(document.querySelectorAll(".tab__content"));
-function turnOn() {
-    this.indexOf("")
+function turnOn(event) {
+    this.indexOf(event.target).className = "tab__content tab__content_active";
 }
 
-
-tabs.forEach(tab => tab.addEventListener("click", turnOn))
+tabs.forEach(tab => tab.addEventListener("click", function(){this.classList.add("tab_active")
+}))
+tabsContent.forEach(tabContent => tabContent.addEventListener("click", turnOn))
