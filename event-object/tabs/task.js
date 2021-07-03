9d49c0ec7b0b6c7b7d7b0a2252496875b0b1a269
Svetlased <svetla-sed2@mail.ru>
2021-07-03 12:37:00 +0200

    Corrections tab remove were added

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,14 +3,15 @@ const tabsContent = Array.from(document.querySelectorAll(".tab__content"));
 
 tabs.forEach(function(tab){
     let count = tabs.indexOf(tab);
-    tab.addEventListener("click", function(){
-        if(tab.classList.contains("tab_active")) {
-            tab.classList.remove("tab_active");
-            tabsContent[count].classList.remove("tab__content_active");
-        } else {
-            tab.classList.add("tab_active");
-            document.querySelector('.tab__content_active').classList.remove('tab__content_active');
-            tabsContent[count].classList.add("tab__content_active");
+    tab.addEventListener("click", function(event) {
+        for (let i = 0; i < tabs.length; i++) {
+            if (tabs.indexOf(event.target) == i) {
+                tabs[i].classList.add('tab_active');
+                tabsContent[i].classList.add('tab__content_active');
+            } else {
+                tabs[i].classList.remove('tab_active');
+                tabsContent[i].classList.remove('tab__content_active');
+            }
         }
 })
 })
